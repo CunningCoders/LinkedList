ca8724d9876aaf7c07cf05ae9bfa971278808c68
Zachary Lee <w.zachary.lee@gmail.com>
2015-07-16 16:12:12 -0500

    created homepage and profile folders, updated createJob, findJobs, jobDetails

---
+++ client/app/findJobs/findJobs.ctrl.js
@@ -0,0 +1,5 @@
+angular.module('app.findJobs', [])
+
+.controller('findJobsController', function($scope, findJobsFactory) {
+	$scope.test = "hey kid! i'm a computer"
+})
---
+++ client/app/profile/profile.fact.js
@@ -0,0 +1,6 @@
+angular.module('app.profile', [])
+
+
+.factory('profileFactory', function() {
+	
+})
