d2d9ec0fb983ab3f07a58a0f45eddc568a2a6659
WesTyler <westyler5@gmail.com>
2015-07-16 11:32:03 -0500

    Set up bower.json and app.js for UI-Router. Create findJobs/

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,41 @@
-app.js
+angular.module('app', [
+  'app.createJob',
+  'app.findJobs',
+  'app.jobDetails',
+  'app.profile',
+  'app.homepage',
+  'ui.router'
+])//Each view's module needs to be pulled in here, along with other dependencies
+
+.config(function($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise("/"); //Re-route unknown to homepage
+
+  $stateProvider
+    .state('createJob', {
+      url: '/createJob',
+      templateUrl: 'createJob/createJob.html',
+      controller: 'createJob/createJob.ctrl.js'
+    })
+    .state('findJobs', {
+      url: '/findJobs',
+      templateUrl: 'findJobs/findJobs.html',
+      controller: 'findJobs/findJobs.ctrl.js'
+    })
+    .state('jobDetails', {
+      url: '/jobDetails',
+      templateUrl: 'jobDetails/jobDetails.html',
+      controller: 'jobDetails/jobDetails.ctrl.js' 
+    })
+    .state('profile', {
+      url: '/profile',
+      templateUrl: 'profile/profile.html',
+      controller: 'profile/profile.ctrl.js'
+    })
+    .state('homepage', {
+      url: '/', 
+      templateUrl: 'homepage/homepage.html',
+      controller: 'homepage/homepage.ctrl.js'
+    })
+})
+//authentication checking here? Like in shortly-angular.
