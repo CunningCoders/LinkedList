8b1dc961d9b29f87adf9345cc5b88f54c3438658
WesTyler <westyler5@gmail.com>
2015-07-17 13:38:59 -0500

    Filter incoming jobs from server by current/pending projects

--- client/app/homepage/homepage.ctrl.js
+++ client/app/homepage/homepage.ctrl.js
@@ -1,14 +1,21 @@
 angular.module('app')
 
 .controller('homepageController', ['$scope', 'jobs', function($scope, jobs) {
-  $scope.currentProjects = jobs.data;
-  $scope.pendingProjects = [
-    { id: 3,
-    title: 'Awesome JS',
-    ownerid: 2,
-    description: 'Make it awsome.',
-    skills: 'Angular, UI-Router, Backbone',
-    coworkers: 'Colin, Brittney, John, Zach' }
-  ];
+  $scope.allProjects = jobs.data;
+  $scope.user = [2,'Wes'];
+  $scope.currentProjects = $scope.allProjects.filter(function(job){
+    var coworkerIds = [];
+    for (var i=0; i<job.coworkers.length; i++) {
+      coworkerIds.push(job.coworkers[i][0])
+    };
+    return job.ownerid[0]===$scope.user[0] || coworkerIds.indexOf($scope.user[0])>=0;
+  });
+  $scope.pendingProjects = $scope.allProjects.filter(function(job){
+    var coworkerIds = [];
+    for (var i=0; i<job.pendingcoworkers.length; i++) {
+      coworkerIds.push(job.pendingcoworkers[i][0])
+    };
+    return coworkerIds.indexOf($scope.user[0])>=0;
+  });;
 
 }])
