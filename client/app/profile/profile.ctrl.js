346b743877005f5b28749bd4849045a4493fd3bc
Zachary Lee <w.zachary.lee@gmail.com>
2015-07-17 15:02:23 -0500

    filled out createJob and profile html, profile html is still in progress

--- client/app/profile/profile.ctrl.js
+++ client/app/profile/profile.ctrl.js
@@ -1,5 +1,36 @@
 angular.module('app') 
 
-.controller('profileController', function($scope) {
-	$scope.test = "stop all the downloadin (Profile View)"
+.controller('profileController', function($scope, profileFactory) {
+	//if logged in, be allowed to edit
+	$scope.editIndex = -1
+	$scope.editObject = {
+		firstName: "",
+		lastName: "",
+		imgSrc: "",
+		from: {city: "", state: ""},
+		phone: "",
+		email: "",
+		jobTitle: "",
+		company: "",
+		description: "",
+		skills: ""
+	}
+	$scope.editingPerson = function(personIndex){
+		//seperate edits from original array, in case of cancel
+	    $scope.editObject = angular.copy($scope.employeeArray[personIndex]); 
+	    $scope.editIndex = personIndex;
+     };
+
+    $scope.cancelEdit = function(){
+        $scope.editIndex = -1;
+    }; 
+    //saveEdit
+    $scope.saveEdit = function(personIndex){
+        profileFactory.updateInfo(personIndex, $scope.editObject);
+        $scope.editIndex = -1;
+    }
+
+	//a button that can connect to linkedin
+
+	//list of jobs that profileperson is currently working on (including his own)
 })
--- client/app/profile/profile.fact.js
+++ client/app/profile/profile.fact.js
@@ -2,5 +2,8 @@ angular.module('app.profile', [])
 
 
 .factory('profileFactory', function() {
+	this.updateInfo = function(personIndex, obj){
+        staffArray.splice(personIndex, 1, obj)
+    }
 	
 })
