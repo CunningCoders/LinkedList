32dc8203e6f9a20308bec874607a92006346df73
WesTyler <westyler5@gmail.com>
2015-07-17 14:09:14 -0500

    Simplify project sorting based on DB schema. Jobs have status=(pending || confirmed)

--- client/app/homepage/homepage.ctrl.js
+++ client/app/homepage/homepage.ctrl.js
@@ -2,20 +2,11 @@ angular.module('app')
 
 .controller('homepageController', ['$scope', 'jobs', function($scope, jobs) {
   $scope.allProjects = jobs.data;
-  $scope.user = [2,'Wes'];
   $scope.currentProjects = $scope.allProjects.filter(function(job){
-    var coworkerIds = [];
-    for (var i=0; i<job.coworkers.length; i++) {
-      coworkerIds.push(job.coworkers[i][0])
-    };
-    return job.ownerid[0]===$scope.user[0] || coworkerIds.indexOf($scope.user[0])>=0;
+    return job.status==='confirmed';
   });
   $scope.pendingProjects = $scope.allProjects.filter(function(job){
-    var coworkerIds = [];
-    for (var i=0; i<job.pendingcoworkers.length; i++) {
-      coworkerIds.push(job.pendingcoworkers[i][0])
-    };
-    return coworkerIds.indexOf($scope.user[0])>=0;
+    return job.status==='pending';
   });;
 
 }])
