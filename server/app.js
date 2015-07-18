1485de7173d25b057905bcf550550386c45d21d2
John McGuirk <johnmac81@gmail.com>
2015-07-17 19:46:58 -0500

    local auth in progress

--- server/app.js
+++ server/app.js
@@ -5,7 +5,39 @@ var passportLocal = require('passport-local');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var db = require('./db');
+//var db = require('./db');
+var pg = require('pg');
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host     : '127.0.0.1',
+    user     : 'postgres',
+    password : 'password',
+    database : 'mydb',
+    port: 5432
+    // debug: true
+  }
+});
+
+
+
+//TESTING
+// app.get('/test', function(req, res){
+//  knex('users').where('username', 'john');
+//    res.send(users.username);
+// });
+
+// knex.select("*").from("users").then(function(users){
+//  //res.end();
+//  console.log({user:users});
+//  return({user:users});
+// });
+
+var pg = require('knex')({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING
+});
 
 app.set('view engine', 'ejs');//for testing
 // set below for rendering views in PROD
@@ -21,22 +53,31 @@ app.use(session({
 }));
 
 app.use(passport.initialize());
-app.use(passport.session())
+app.use(passport.session());
 
 passport.use(new passportLocal.Strategy(function(username, password, done){
   //DB connection in PROD
-  if(username === password){
-    done(null, { id: username, name: username });
+  knex('users').where({
+      username: this.username
+  }).select('username')
+
+  //db.initDB();
+  //var results = pg.query('select * from Users where username = "john1";');
+  //console.log(results);
+  if(knex('users').where({
+      username: this.username
+  }).select('username')){
+    done(null, { user: username, user: password });
     console.log('authenticated');
   } else {
-    done(null, error);
+    done(null, false);
     console.log('not authenticated');
   }
 }));
 
 passport.serializeUser(function(user, done){
   //query db here
-  done(null, user.id);
+  done(null, user.username);
 });
 
 passport.deserializeUser(function(id, done){
