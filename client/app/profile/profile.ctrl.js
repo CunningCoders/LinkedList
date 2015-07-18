a70c21bfabb86b751d761947874f8254411964ea
Zachary Lee <w.zachary.lee@gmail.com>
2015-07-17 19:36:32 -0500

    filled out profile view

--- client/app/profile/profile.ctrl.js
+++ client/app/profile/profile.ctrl.js
@@ -1,36 +1,70 @@
-angular.module('app') 
+angular.module('app')
 
-.controller('profileController', function($scope, profileFactory) {
-	//if logged in, be allowed to edit
-	$scope.editIndex = -1
-	$scope.editObject = {
-		firstName: "",
-		lastName: "",
-		imgSrc: "",
-		from: {city: "", state: ""},
-		phone: "",
-		email: "",
-		jobTitle: "",
-		company: "",
-		description: "",
-		skills: ""
-	}
-	$scope.editingPerson = function(personIndex){
-		//seperate edits from original array, in case of cancel
-	    $scope.editObject = angular.copy($scope.employeeArray[personIndex]); 
-	    $scope.editIndex = personIndex;
-     };
-
-    $scope.cancelEdit = function(){
-        $scope.editIndex = -1;
-    }; 
-    //saveEdit
-    $scope.saveEdit = function(personIndex){
-        profileFactory.updateInfo(personIndex, $scope.editObject);
-        $scope.editIndex = -1;
+.controller('profileController', ['$scope' ,function($scope) {
+  //if logged in, be allowed to edit
+  $scope.editObject = {
+      firstName: "zack",
+      lastName: "lee",
+      imgSrc: "",
+      from: {
+        city: "houston",
+        state: "texas"
+      },
+      phone: "7132021535",
+      email: "",
+      jobTitle: "ceo",
+      company: "squakermare",
+      description: "",
+      skills: ""
     }
+    //a button that can connect to linkedin
 
-	//a button that can connect to linkedin
+  //list of jobs that profileperson is currently working on (including his own)
+}])
 
-	//list of jobs that profileperson is currently working on (including his own)
-})
+.directive("clickToEdit", function() {
+  var editorTemplate = '' +
+    '<div class="click-to-edit">' +
+    '<div ng-hide="view.editorEnabled">' +
+    '{{value}} ' +
+    '<a class="button tiny" ng-click="enableEditor()">Edit</a>' +
+    '</div>' +
+    '<div ng-show="view.editorEnabled">' +
+    '<input type="text" class="small-12.columns" ng-model="view.editableValue">' +
+    '<a class="button tiny" href="#" ng-click="save()">Save</a>' +
+    ' or ' +
+    '<a class="button tiny" ng-click="disableEditor()">cancel</a>' +
+    '</div>' +
+    '</div>';
+  return {
+    restrict: "A",
+    replace: true,
+    template: editorTemplate,
+    scope: {
+      value: "=clickToEdit",
+    },
+    link: function(scope, element, attrs) {
+      scope.view = {
+        editableValue: scope.value,
+        editorEnabled: false
+      };
+      scope.enableEditor = function() {
+        scope.view.editorEnabled = true;
+        scope.view.editableValue = scope.value;
+        setTimeout(function() {
+          element.find('input')[0].focus();
+          //element.find('input').focus().select(); // w/ jQuery
+        });
+      };
+
+      scope.disableEditor = function() {
+        scope.view.editorEnabled = false;
+      };
+
+      scope.save = function() {
+        scope.value = scope.view.editableValue;
+        scope.disableEditor();
+      };
+    }
+  };
+});
--- client/app/profile/profile.fact.js
+++ client/app/profile/profile.fact.js
@@ -2,8 +2,5 @@ angular.module('app.profile', [])
 
 
 .factory('profileFactory', function() {
-	this.updateInfo = function(personIndex, obj){
-        staffArray.splice(personIndex, 1, obj)
-    }
-	
+
 })
