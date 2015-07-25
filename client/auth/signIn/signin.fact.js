82f35e0b11268a46c2b21cd8c7b63fd4fd0cdba5
Brittany Schroeder <bryttni.schroeder@gmail.com>
2015-07-25 00:55:46 -0500

    Cleaning up signin and signup factories

--- client/auth/signIn/signin.fact.js
+++ client/auth/signIn/signin.fact.js
@@ -1,39 +1,19 @@
 angular.module('auth')
 
-.factory('signInFactory', function($scope, $http){
-	//auth.signin = function(username, password, callback) {
-	  //user.signin(user.username, user.password)
-      //.then()
-	//}
-	var checkSignin = function($q, $timeout, $http, $location, $rootScope) {
-        var deferred = $q.defer();
+.factory('signInFactory', function(){
+	$http({
+        method: 'GET',
+        url: '/signin',
+        params: {}
+    })
+    .success(function(data, status, headers, config) {
 
-        $http.get('/signin').success(function(user){
-            if (user !== '0')
-                deferred.resolve();
+    }).
+    error(function(data, status, headers, config){
 
-            else {
-                $rootScope.message = 'Please sign in.';
-                deferred.reject();
-                $location.url('/signin')
-            }
-        })
-
-        return deferred.promise;
-
-	}
+    })
+    
 });
 
 
-//get request - get all users - pass username
-//for loop, see if present in response
-//$http
-//url directions - endpoints
 
-	// $http.get('/signin').
-	//   success(function(response){
-	//   	$scope.
-	//   }).
-	//   error(function(response){
-
-	//   });
