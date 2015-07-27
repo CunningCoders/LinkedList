4d307dca206ee5983c2a297fccfb29128b5733f6
Zachary Lee <w.zachary.lee@gmail.com>
2015-07-27 11:35:30 -0500

    a lot of changes to css, and profile

--- client/app/jobDetails/jobDetails.ctrl.js
+++ client/app/jobDetails/jobDetails.ctrl.js
@@ -1,7 +1,7 @@
 angular.module('app')
 
 .controller('jobDetailsController', ['$scope', 'job', function($scope, job) {
-	$scope.project = job;
+  $scope.project = job;
   $scope.skills = JSON.parse(job.skills);
   $scope.coworkers = job.coworkers;
 
--- client/app/profile/profile.fact.js
+++ client/app/profile/profile.fact.js
@@ -1,6 +1,29 @@
-angular.module('app.profile', [])
+angular.module('app')
 
 
 .factory('profileFactory', function() {
+  var editObject = [
+    {
+      firstName: "zack",
+      lastName: "lee",
+      imgSrc: "http://1funny.com/wp-content/uploads/2010/09/surfing-grandma.jpg",
+      from: {
+        city: "houston",
+        state: "texas"
+      },
+      phone: "7132021535",
+      email: "",
+      jobTitle: "ceo",
+      company: "squakermare",
+      description: "just a grandma and her surfboard",
+      skills: ""
+    }
+  ]
+  this.getProfile = function(){
+    return editObject[0]
+  };
+  this.updateInfo = function(personIndex, obj){
+            staffArray.splice(personIndex, 1, obj)
+        }
 
 })
